e312a972fb252e962459cbcdf10ddc9d6064e483
Nicolas Andrew Burnett <nicolasandrewburnett@gmail.com>
2021-02-08 04:51:33 +0000

    Revert "- modified installation scripts to account for host-system changes"
    This reverts commit a219a924e0e0226997d2bb5a7d743dd28f6ee1fd.
    
    Rewriting Commit Message

--- install/install_profile.sh
+++ install/install_profile.sh
@@ -2,7 +2,7 @@ echo 'Beginning profile installation...'
 cd ~ || exit
 mkdir './.install_profile_temp/'
 cd './.install_profile_temp/'
-curl -o 'profile.txt' 192.168.1.92:3000/clitools/bash/profile
+curl -o 'profile.txt' localhost:3000/clitools/bash/profile
 cat 'profile.txt' >> ~/.bash_profile
 echo 'Cleaning Up...'
 cd '..'
